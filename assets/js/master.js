56081bab4e929b799043e35fc94deac54e6ab546
farnazdarvish <f.darvish1996@gmail.com>
2024-11-05 23:31:59 +0330

    input for creat acc completed,errors completed

--- assets/js/master.js
+++ assets/js/master.js
@@ -8,17 +8,18 @@ const register = document.querySelector('.register')
 ///selecting login pinkcard:
 const login = document.querySelector('.log')
 /// selecting input by id
-let username = document.querySelector('#username')
-let username2 = document.querySelector('#username2')
 let email = document.querySelector('#email')
 let password = document.querySelector('#password')
 let password2 = document.querySelector('#password2')
 let confirmpassword = document.querySelector('#confirmpassword')
 
-////selecting forms:
-let form1 = document.querySelectorAll('.form1>input')
-let form2 = document.querySelectorAll('.form2>input')
 
+////selecting forms:
+// let form1 = document.forms['form1']
+let form2 = document.querySelectorAll('.form2>input')
+const validation = document.querySelector('.valid')
+const emailValidation = document.querySelector('.e-valid')
+const pass = document.querySelector('.pass')
 
 
 
@@ -28,6 +29,11 @@ btnlog.addEventListener('click', function () {
     register.style.display = 'none'
     login.style.display = 'block'
     login.style.transform = 'translateX(-100%)'
+    document.forms['form1']['username'].value = ""
+    document.forms['form1']['email'].value = ""
+    document.forms['form1']['password'].value = ""
+    document.forms['form1']['confirmpassword'].value = ""
+
 
 
 
@@ -49,27 +55,14 @@ username.addEventListener('focus', function () {
 username.addEventListener('blur', function () {
     username.style.borderColor = '#F6F6F6'
     username.placeholder = 'username'
-})
-username.addEventListener('input', function (e) {
+    // console.log('username value ', username?.value)
+    if (username.value.search(/^[a-z0-9_-]{3,15}$/) == -1) {
+        validation.style.display = 'block'
+    } else {
+        validation.style.display = 'none'
 
-    if (e.target.value.search(/^[a-z0-9_-]{3,15}$/) == -1) {
-        // e.target.value = e.target.value.slice(0, e.target.value.length - 1)
-        alert('the user name is invalid')
     }
-
 })
-
-//password
-password.addEventListener('focus', function () {
-    password.style.borderColor = '#CE7D88'
-    password.placeholder = ''
-
-})
-password.addEventListener('blur', function () {
-    password.style.borderColor = '#F6F6F6'
-    password.placeholder = 'password'
-})
-
 ////email
 email.addEventListener('focus', function () {
     email.style.borderColor = '#CE7D88'
@@ -79,7 +72,50 @@ email.addEventListener('focus', function () {
 email.addEventListener('blur', function () {
     email.style.borderColor = '#F6F6F6'
     email.placeholder = 'email'
+    if (email.value.search(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/) == -1) {
+        emailValidation.style.display = 'block'
+    } else {
+        emailValidation.style.display = 'none'
+
+    }
 })
+
+//password
+const line = document.querySelector('.line')
+
+password.addEventListener('focus', function () {
+    password.style.borderColor = '#CE7D88'
+    password.placeholder = ''
+
+})
+password.addEventListener('blur', function () {
+    password.style.borderColor = '#F6F6F6'
+    password.placeholder = 'password'
+})
+password.addEventListener('input', () => {
+    let pass = password.value
+    let flag = 0
+    pass.search(/[0-9]/) >= 0 && flag++
+    pass.search(/[a-z]/) >= 0 && flag++
+    pass.search(/[A-Z]/) >= 0 && flag++
+    pass.search(/[@#$%^&&**()]/) >= 0 && flag++
+    pass.length >= 8 && flag++
+
+    switch (flag) {
+        case 0: line.style.width = '0%'; line.style.background = 'black'; break;
+        case 1: line.style.width = '15%'; line.style.background = 'red'; break;
+        case 2: line.style.width = '30%'; line.style.background = 'orange'; break;
+        case 3: line.style.width = '45%'; line.style.background = 'yellow'; break;
+        case 4: line.style.width = '65%'; line.style.background = 'yellowgreen'; break;
+        case 5: line.style.width = '80%'; line.style.background = 'green'; break;
+    }
+    console.log(line +
+        'salam'
+    );
+
+})
+
+
 
 ////confirmpassword
 confirmpassword.addEventListener('focus', function () {
@@ -90,6 +126,12 @@ confirmpassword.addEventListener('focus', function () {
 confirmpassword.addEventListener('blur', function () {
     confirmpassword.style.borderColor = '#F6F6F6'
     confirmpassword.placeholder = 'confirm password'
+    if (confirmpassword.value !== password.value) {
+        pass.style.display = 'block'
+    } else {
+        pass.style.display = 'none'
+
+    }
 })
 
 
@@ -116,4 +158,5 @@ password2.addEventListener('focus', function () {
 username2.addEventListener('blur', function () {
     password2.style.borderColor = '#F6F6F6'
     password2.placeholder = 'password'
+
 })
