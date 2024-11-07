a4818bb7a92711cab3bbdee9bec83ed214e6d5b3
farnazdarvish <f.darvish1996@gmail.com>
2024-11-08 00:15:15 +0330

    errors fixed

--- assets/js/master.js
+++ assets/js/master.js
@@ -13,19 +13,23 @@ let email = document.querySelector('#email')
 let password = document.querySelector('#password')
 let password2 = document.querySelector('#password2')
 let username2 = document.querySelector('#username2')
-
 let confirmpassword = document.querySelector('#confirmpassword')
-
-
 ////selecting forms:
-// let form1 = document.forms['form1']
-let form2 = document.querySelectorAll('.form2>input')
 const validation = document.querySelector('.valid')
 const emailValidation = document.querySelector('.e-valid')
 const pass = document.querySelector('.pass')
 const validation2 = document.querySelector('.valid2')
 const pass2 = document.querySelector('.pass2')
 const line = document.querySelector('.line')
+/////////////input selecting
+let f1User = document.forms['form1']['username']
+let f1Email = document.forms['form1']['email']
+let f1Pass = document.forms['form1']['password']
+let f1Confirm = document.forms['form1']['confirmpassword']
+let f2User = document.forms['form2']['username2']
+let f2Pass = document.forms['form2']['password2']
+
+
 
 
 
@@ -36,28 +40,23 @@ btnlog.addEventListener('click', function () {
     register.style.display = 'none'
     login.style.display = 'block'
     login.style.transform = 'translateX(-100%)'
-    document.forms['form2']['username2'].value = ""
-    document.forms['form2']['password2'].value = ""
+    f2User.value = ""
+    f2Pass.value = ""
     validation2.style.display = 'none'
     pass2.style.display = 'none'
-
-
-
-
-
-
-
+   
 
 })
+
 
 btnsign.addEventListener('click', function () {
     login.style.display = 'none'
     register.style.display = 'block'
     register.style.transform = 'translateX(0)'
-    document.forms['form1']['username'].value = ""
-    document.forms['form1']['email'].value = ""
-    document.forms['form1']['password'].value = ""
-    document.forms['form1']['confirmpassword'].value = ""
+    f1User.value = ""
+    f1Email.value = ""
+    f1Pass.value = ""
+    f1Confirm.value = ""
     validation.style.display = 'none'
     emailValidation.style.display = 'none'
     pass.style.display = 'none'
@@ -66,7 +65,7 @@ btnsign.addEventListener('click', function () {
 
 })
 
-
+let usernameIsCorrect = false;
 ////username
 username.addEventListener('focus', function () {
     username.style.borderColor = '#CE7D88'
@@ -76,15 +75,18 @@ username.addEventListener('focus', function () {
 username.addEventListener('blur', function () {
     username.style.borderColor = '#F6F6F6'
     username.placeholder = 'username'
-    // console.log('username value ', username?.value)
     if (username.value.search(/^[a-z0-9_-]{3,15}$/) == -1) {
+        usernameIsCorrect = false
         validation.style.display = 'block'
+
     } else {
+        usernameIsCorrect = true
         validation.style.display = 'none'
 
     }
 })
 ////email
+let emailIsCorrect = false
 email.addEventListener('focus', function () {
     email.style.borderColor = '#CE7D88'
     email.placeholder = ''
@@ -94,15 +96,19 @@ email.addEventListener('blur', function () {
     email.style.borderColor = '#F6F6F6'
     email.placeholder = 'email'
     if (email.value.search(/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/) == -1) {
+        emailIsCorrect = false
         emailValidation.style.display = 'block'
     } else {
         emailValidation.style.display = 'none'
+        emailIsCorrect = true
+
 
     }
 })
 
-//password
 
+//password
+let passwordIsCorrect = false
 password.addEventListener('focus', function () {
     password.style.borderColor = '#CE7D88'
     password.placeholder = ''
@@ -129,9 +135,6 @@ password.addEventListener('input', () => {
         case 4: line.style.width = '65%'; line.style.background = 'yellowgreen'; break;
         case 5: line.style.width = '80%'; line.style.background = 'green'; break;
     }
-    console.log(line +
-        'salam'
-    );
 
 })
 
@@ -150,6 +153,8 @@ confirmpassword.addEventListener('blur', function () {
         pass.style.display = 'block'
     } else {
         pass.style.display = 'none'
+        passwordIsCorrect = true
+
 
     }
 })
@@ -196,13 +201,16 @@ password2.addEventListener('blur', function () {
 const checkBox = document.querySelector('#checkBox')
 let usernameSave = localStorage.getItem('data1')
 let passwordSave = localStorage.getItem('data2')
-if (usernameSave && passwordSave != null) {
-    checkBox.addEventListener('focus',()=>{
-        username2=username.value 
-        password.value = password2
-    
-    })
-}
+checkBox.addEventListener('focus', () => {
+    if (usernameSave && passwordSave != null) {
+
+        document.forms['form2']['username2'].value = username.value
+        document.forms['form2']['password2'].value = password.value
+
+
+    }
+})
+
 function mylocal() {
     let temp1 = username.value
     let temp2 = password.value
@@ -211,8 +219,22 @@ function mylocal() {
 
 
 }
-const btnreg = document.querySelector('.btnreg')
+const btnreg = document.querySelector('.btnreg>button')
 btnreg.addEventListener('click', () => {
     mylocal()
-})
+
+    if (f1User.value && f1Pass.value && f1Email.value && f1Confirm.value != "") {
+
+    } else {
+        alert('adam bash')
+    }
+    if ((usernameIsCorrect && passwordIsCorrect && emailIsCorrect)) {
+        alert('account created...')
+    } else {
+        alert('check required value')
+    }
+
+}
+
+)
 
